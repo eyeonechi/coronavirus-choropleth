a4a7a8d6bc6dc4d22583425e6f50738d473b67f9
eyeonechi <xenzaki@msn.com>
2020-03-10 01:36:27 +0800

    Include source files

---
+++ src/server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var fetch = require('node-fetch');
+
+var server = app.listen(8080, '0.0.0.0', function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Server listening at http://%s:%s", host, port)
+})
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  fetch(
+    'https://corona.lmao.ninja/countries',
+    {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }
+  ).then(res => res.json())
+  .then(data => {
+    res.render('index', {cases: JSON.stringify(data)});
+  })
+  .catch(err => {
+    console.error(err);
+    res.render('Unable to fetch latest data');
+  });
+});
