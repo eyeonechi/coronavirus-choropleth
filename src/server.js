928fb1504cf898daf246cb14656de1e848bdde55
eyeonechi <xenzaki@msn.com>
2020-03-14 22:51:33 +0800

    Refactor geojson-data matching back to client side

--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var fetch = require('node-fetch');
 var csv = require('csvtojson');
-var fs = require('fs');
 
 var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address
@@ -10,38 +9,11 @@ var server = app.listen(process.env.PORT || 3000, function () {
   console.log("Server listening at http://%s:%s", host, port)
 });
 
-var data = JSON.parse(fs.readFileSync('data/countries.geo.json', 'utf8'));
+// var data = JSON.parse(fs.readFileSync('data/countries.geo.json', 'utf8'));
 
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
-
-function matchCountry(name, cases) {
-  for (let i = 0; i < cases.length; i ++) {
-    if (cases[i].country === 'USA' && name === 'United States') {
-      return cases[i];
-    } else if (cases[i].country === 'UK' && name === 'United Kingdom') {
-      return cases[i];
-    } else if (cases[i].country === 'UAE' && name === 'United Arab Emirates') {
-      return cases[i];
-    } else if (cases[i].country === 'Czechia' && name === 'Czech Rep.') {
-      return cases[i];
-    } else if (cases[i].country === 'Bosnia and Herzegovina' && name === 'Bosnia and Herz.') {
-      return cases[i];
-    } else if (cases[i].country === 'N. Korea' && name === 'Dem. Rep. Korea') {
-      return cases[i];
-    } else if (cases[i].country === 'S. Korea' && name === 'Korea') {
-      return cases[i];
-    } else if (cases[i].country === 'Laos' && name === 'Lao PDR') {
-      return cases[i];
-    } else if (cases[i].country === 'Dominican Republic' && name === 'Dominican Rep.') {
-      return cases[i];
-    } else if (cases[i].country === name) {
-      return cases[i];
-    }
-  }
-  return null;
-}
 
 app.get('/', function (req, res) {
   // csv()
@@ -60,29 +32,7 @@ app.get('/', function (req, res) {
     }
   ).then(res => res.json())
   .then(cases => {
-
-    for (let j = 0; j < data.features.length; j ++) {
-      data.features[j].properties.cases = 0;
-      data.features[j].properties.todayCases = 0;
-      data.features[j].properties.deaths = 0;
-      data.features[j].properties.todayDeaths = 0;
-      data.features[j].properties.recovered = 0;
-      data.features[j].properties.critical = 0;
-      let match = matchCountry(data.features[j].properties.name, cases);
-      if (match !== null) {
-        data.features[j].properties.cases = match.cases;
-        data.features[j].properties.todayCases = match.todayCases;
-        data.features[j].properties.deaths = match.deaths;
-        data.features[j].properties.todayDeaths = match.todayDeaths;
-        data.features[j].properties.recovered = match.recovered;
-        data.features[j].properties.critical = match.critical;
-      }
-      // else {
-      //   console.log(data.features[j].properties.name);
-      // }
-    }
-
-    res.render('index', {data: JSON.stringify(data)});
+    res.render('index', {cases: JSON.stringify(cases)});
   })
   .catch(err => {
     console.error(err);
