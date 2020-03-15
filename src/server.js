6c945683aeba515427407f5dd03fc14781b69199
eyeonechi <xenzaki@msn.com>
2020-03-15 18:00:36 +0800

    Add toggles for SARS and MERS data

--- src/server.js
+++ src/server.js
@@ -16,26 +16,32 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  // csv()
-  // .fromFile('data/MERS.csv')
-  // .then((data) => {
-  //   res.render('index', {cases: JSON.stringify(data)});
-  // });
-
-  fetch(
-    'https://corona.lmao.ninja/countries',
-    {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    }
-  ).then(res => res.json())
-  .then(cases => {
-    res.render('index', {cases: JSON.stringify(cases)});
-  })
-  .catch(err => {
-    console.error(err);
-    res.render('Unable to fetch latest data');
+  csv()
+  .fromFile('data/MERS.csv')
+  .then((mers) => {
+    csv()
+    .fromFile('data/SARS.csv')
+    .then((sars) => {
+      fetch(
+        'https://corona.lmao.ninja/countries',
+        {
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+        }
+      ).then(res => res.json())
+      .then(covid19 => {
+        res.render('index', {
+          covid19: JSON.stringify(covid19),
+          sars: JSON.stringify(sars),
+          mers: JSON.stringify(mers)
+        });
+      })
+      .catch(err => {
+        console.error(err);
+        res.render('Unable to fetch latest data');
+      });
+    });
   });
 });
